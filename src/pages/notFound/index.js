c59fa67b07a59174da1a2a921bccb1232c7da0c6
sepideh Borazjani <sepide.bri@gmail.com>
2022-02-18 09:36:53 +0330

    styled-not-found added

--- src/pages/notFound/index.js
+++ src/pages/notFound/index.js
@@ -1,14 +1,24 @@
-import { Fragment } from "react"
-import { Link } from "react-router-dom"
+import { Link } from "react-router-dom";
+import { Row, Col } from "antd";
+import { Style } from "./style";
 
 export function NotFound(){
     return(
-        <Fragment>
-            <nav>
-                <Link to="/">Home</Link>
-            </nav>
-            <p>404</p>
-        </Fragment>    
+        <Style>
+            <Col>
+                <Row align="middle" justify="center">
+                <Row align="middle" justify="center" >
+                    <Col><div className="error">404</div></Col>
+                    <Col><div className="error-content">This page could not be found.</div></Col>
+                </Row>
+                </Row>
+                <Row align="middle" justify="center">
+                <nav>
+                    <Link to="/"><span className="return-home"><span className="home-contet">Home</span><span><h6>click on me!</h6></span></span></Link>
+                </nav>
+                </Row>
+            </Col>
+        </Style>
     )
 }
 export default NotFound
