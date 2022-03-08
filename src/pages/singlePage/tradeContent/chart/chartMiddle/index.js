93a2a52584ce30c45c89feb6fc7508386be84bf9
sepideh Borazjani <sepide.bri@gmail.com>
2022-03-08 17:28:11 +0330

    :heart: chart added

--- src/pages/singlePage/tradeContent/chart/chartMiddle/index.js
+++ src/pages/singlePage/tradeContent/chart/chartMiddle/index.js
@@ -1,16 +1,49 @@
 import Style from "./style";
-import { Line } from 'react-chartjs-2';
 import  Chart  from "chart.js/auto";
-import { CategoryScale, Interaction } from "chart.js";
 import api from "utils/api";
 import { useEffect, useState } from "react";
+import timestampToHour from "utils/api/timestampToTime";
 import { Link, useParams } from "react-router-dom";
+import {
+    Chart as ChartJS,
+    CategoryScale,
+    LinearScale,
+    PointElement,
+    LineElement,
+    Title,
+    Tooltip,
+    Legend,
+  } from 'chart.js';
+  import { Line } from 'react-chartjs-2';
+ChartJS.register(
+  CategoryScale,
+  LinearScale,
+  PointElement,
+  LineElement,
+  Title,
+  Tooltip,
+  Legend
+);
+export const options = {
+    responsive: true,
+    plugins: {
+      legend: {
+        position: 'top',
+      },
+      title: {
+        display: true,
+        text: 'Chart.js Line Chart',
+      },
+    },
+  };
+  
 
-Chart.register(CategoryScale);
+  
 export function ChartMiddle(){
     const [assets, setAssets] = useState([]);
     const [item, setItem]= useState({})
     const {id} = useParams();
+    
     useEffect(function(){
         async function getApi(){
             try{
@@ -24,13 +57,36 @@ export function ChartMiddle(){
         }
         getApi();
     }, []);
+    function manipulateTime(){
+        return assets.map((item) => {
+            return timestampToHour(item.time)
+        })
+    };
+    function manipulatePrice(){
+        return assets.map((item) => {
+            return item.priceUsd
+        })
+    };
+    const labels = manipulateTime();
+    const data = {
+        labels,
+        datasets: [
+          {
+            label: 'Dataset 1',
+            data: manipulatePrice(),
+            borderColor: 'rgb(255, 99, 132)',
+            backgroundColor: 'rgba(255, 99, 132, 0.5)',
+          },
+        ],
+      };
+    
     function changeToAmPm(){
         const time = new Date();
         time.toLocaleString('en-US', { hour: 'numeric', hour12: true });
     };
     return(
         <Style>
-        
+        <Line options={options} data={data} />
         </Style>
     )
 };
